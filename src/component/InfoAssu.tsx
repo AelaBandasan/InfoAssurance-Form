120186b708ab76fe1818b62cafdb67379b59c2cb
AelaBandasan <bandasanmichaela@gmail.com>
2025-05-12 01:37:23 +0800

    resized text

--- src/component/InfoAssu.tsx
+++ src/component/InfoAssu.tsx
@@ -15,12 +15,15 @@ const InfoAssu: React.FC = () => {
   };
 
   return (
-    <Container
+    <Container className='shadow'
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        width: '100vw',
+        width: 500,
+        marginTop: 50,
+        backgroundColor: '#FBFBFB',
+        borderRadius: 10
       }}>
       <Form>
         <Form.Group controlId="cipherType">
@@ -29,9 +32,11 @@ const InfoAssu: React.FC = () => {
                 alignItems: 'center', 
                 display: 'flex', 
                 fontWeight: 700, 
-                fontSize: 20}}>Cryptogram Algorithm</Form.Label>
+                fontSize: 22,
+                paddingTop: 30,
+                }}>Cryptogram Algorithm</Form.Label>
           <div style={{ position: 'relative', width: '100%' }}>
-            <Form.Control as="select" style={{ paddingRight: '30px', width: 400, height: 50}}>
+            <Form.Control as="select" style={{ fontSize: 17, paddingRight: '30px', width: 400, height: 50, borderWidth: 2, borderColor: '#4C585B'}}>
               <option>Vigenère Cipher </option>
               <option>Standard Columnar Transposition Cipher</option>
             </Form.Control>
@@ -45,12 +50,12 @@ const InfoAssu: React.FC = () => {
                     fontSize: "20px" // Adjusts the size of the icon
                 }}>
 
-                </i>g
+                </i>
           </div>
         </Form.Group>
 
         <Form.Group controlId="plaintext">
-          <Form.Label style={{fontWeight: 600, fontSize: 17, paddingTop: 10}}>Plaintext</Form.Label>
+          <Form.Label style={{fontWeight: 600, fontSize: 18, paddingTop: 10, }}>Plaintext</Form.Label>
           <Form.Control
             as="textarea"
             rows={3}
@@ -58,41 +63,41 @@ const InfoAssu: React.FC = () => {
             placeholder='Enter text to encrypt or decrypt'
             onChange={(e) => setPlaintext(e.target.value)}
 
-            style={{height: 150}}
+            style={{fontSize: 17, color: 'black', height: 150, borderWidth: 2, borderColor: '#4C585B'}}
           />
         </Form.Group>
 
         <Form.Group controlId="key">
-          <Form.Label style={{fontWeight: 600, fontSize: 17, paddingTop: 10}}>Key</Form.Label>
+          <Form.Label style={{fontWeight: 600, fontSize: 18, paddingTop: 10}}>Key</Form.Label>
           <Form.Control
             type="text"
             value={key}
             placeholder='Enter encryption/decryption key'
             onChange={(e) => setKey(e.target.value)}
 
-            style={{height: 50}}
+            style={{fontSize: 17, color: 'black',height: 50, borderWidth: 2, borderColor: '#4C585B'}}
           />
         </Form.Group>
 
         <Row className="g-0 d-flex justify-content-between"  style={{paddingTop: 15}}>
           <Col>
             <Button variant="primary" onClick={handleEncode}
-                style={{width: 195, borderRadius: 40, height: 45, backgroundColor: '#111F4D'}}>
+                style={{width: 195, borderRadius: 40, height: 45, backgroundColor: '#111F4D', fontWeight: 600, fontSize: 17}}>
               Encode
             </Button>
           </Col>
           <Col>
             <Button variant="primary" onClick={handleDecode}
-                style={{width: 195, borderRadius: 40, height: 45, backgroundColor: '#111F4D'}}>
+                style={{width: 195, borderRadius: 40, height: 45, backgroundColor: '#111F4D', fontWeight: 600, fontSize: 17}}>
               Decode
             </Button>
           </Col>
         </Row>
 
         <Form.Group controlId="output">
-          <Form.Label style={{fontWeight: 600, fontSize: 17, paddingTop: 10}}>Output</Form.Label>
+          <Form.Label style={{fontWeight: 600, fontSize: 18, paddingTop: 10}}>Output</Form.Label>
           <Form.Control as="textarea" rows={3} value={output} readOnly
-          style={{height: 150}} />
+          style={{fontSize: 17,height: 150, borderWidth: 2, color: 'black',borderColor: '#4C585B', marginBottom: 50}} />
         </Form.Group>
       </Form>
     </Container>
